945ed5013b8641c9cc06ff4c2552253e9ab43e17
vtudn <81026582+vtudn@users.noreply.github.com>
2024-01-11 23:11:01 +0700

    add push relabel

--- main.cpp
+++ main.cpp
@@ -6,6 +6,70 @@
 
 #define TOP_K 5
 
+bool PushRelabel_canPush(std::vector<std::vector<int>> &capacity, int src, int sink, std::vector<int> &parent)
+{
+	int N = capacity.size();
+	std::vector<int> vis(N, 0);
+	vis[src] = 1;
+	parent[src] = -1;
+	std::queue<int> q;
+	q.push(src);
+	while (!q.empty())
+	{
+		int cnode = q.front();
+		q.pop();
+		for (int i = 0; i < N; i++)
+		{
+			if (vis[i] == 0 && capacity[cnode][i] > 0)
+			{
+				vis[i] = 1;
+				parent[i] = cnode;
+				q.push(i);
+			}
+		}
+	}
+	if (vis[sink])
+		return true;
+	return false;
+}
+
+int PushRelabel_FIFO(std::vector<std::vector<int>> &capacity, int src, int sink)
+{
+	int N = capacity.size();
+	std::vector<int> parent(N);
+	int max_flow = 0;
+	while (PushRelabel_canPush(capacity, src, sink, parent))
+	{
+		int curr_flow = INT_MAX;
+		int temp = sink;
+		while (temp != src)
+		{
+			curr_flow = std::min(curr_flow, capacity[parent[temp]][temp]);
+			temp = parent[temp];
+		}
+		temp = sink;
+		while (temp != src)
+		{
+			capacity[parent[temp]][temp] -= curr_flow;
+			temp = parent[temp];
+		}
+		max_flow += curr_flow;
+	}
+	return max_flow;
+}
+
+int PushRelabel_dev(int src, int sink)
+{
+	std::vector<std::vector<int>> capacity_abs = {
+		{0, 16, 13, 0, 0, 0},
+		{0, 0, 10, 12, 0, 0},
+		{0, 4, 0, 0, 14, 0},
+		{0, 0, 9, 0, 0, 20},
+		{0, 0, 0, 7, 0, 4},
+		{0, 0, 0, 0, 0, 0}};
+	return PushRelabel_FIFO(capacity_abs, src, sink);
+}
+
 void testDijkstra() // testing only
 {
 	Graph *my_graph_pt = new Graph("data/danYen");
@@ -68,6 +132,7 @@ int main(int argc, char *argv[])
 		std::cout << "The input arguments are wrong. Please try again.\n";
 		return 1;
 	}
+	std::cout << "MAX FLOW: " << PushRelabel_dev(0, TOP_K) << '\n'; // in development state
 	std::cout << "TOP " << TOP_K << " SHORTEST PATHS:\n";
 	std::string cfg_fileName = argv[1];
 	runDSC(cfg_fileName);
